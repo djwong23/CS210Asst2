9c02a93ab7ae1c490517f104e6774aee6c8d1ed7
Derek Wong <djwong23@gmail.com>
2022-03-25 01:18:57 -0400

    finished 2.3

--- pokemon.py
+++ pokemon.py
@@ -101,14 +101,52 @@ def pokemon():
                 else:
                     out.write(f', {p}')
         out = open('pokemon5.txt', 'w')
-        out.write(f'Average hit point for Pokemons of stage 3.0 = {round(sum_stage_3/num_stage_3, 1)}')
+        out.write(f'Average hit point for Pokemons of stage 3.0 = {round(sum_stage_3 / num_stage_3, 1)}')
 
 
+def covid():
+    reader = csv.DictReader(open('covidTrain.csv'))
+    provinces = {}
+    for row in reader:
+        p = row['province']
+        if p not in provinces:
+            provinces[p] = [0 for _ in range(4)]
+        lat = row['latitude']
+        long = row['longitude']
+        if lat != 'NaN':
+            provinces[p][0] += float(lat)
+            provinces[p][1] += 1
+        if long != 'NaN':
+            provinces[p][2] += float(long)
+            provinces[p][3] += 1
+    with open('covidResult.csv', 'w', newline='') as covid_result:
+        reader = csv.DictReader(open('covidTrain.csv'))
+        writer = csv.DictWriter(covid_result, fieldnames=reader.fieldnames, delimiter=',')
+        writer.writeheader()
+        for row in reader:
+            if '-' in row['age']:
+                nums = row['age'].split('-')
+                row['age'] = round((float(nums[0]) + float(nums[1])) / 2)
 
+            dates = row['date_onset_symptoms'].split('.')
+            row['date_onset_symptoms'] = f'{dates[1]}.{dates[0]}.{dates[2]}'
+            dates = row['date_admission_hospital'].split('.')
+            row['date_admission_hospital'] = f'{dates[1]}.{dates[0]}.{dates[2]}'
+            dates = row['date_confirmation'].split('.')
+            row['date_confirmation'] = f'{dates[1]}.{dates[0]}.{dates[2]}'
+
+            if row['latitude'] == 'NaN':
+                province = provinces[row['province']]
+                row['latitude'] = round(province[0] / province[1], 2)
+            if row['longitude'] == 'NaN':
+                province = provinces[row['province']]
+                row['longitude'] = round(province[2] / province[3], 2)
+            writer.writerow(row)
 
 
 def main():
-    pokemon()
+    # pokemon()
+    covid()
 
 
 if __name__ == '__main__':
